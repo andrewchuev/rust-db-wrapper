417323dbcf81ecaf2cc0fee4817faa379de6703f
Andrew A. Chuev <andrew.chuev@gmail.com>
2024-10-17 19:54:22 +0300

    Add fetch_one and fetch_all handlers

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,29 @@ use bigdecimal::BigDecimal;
 use sqlx::mysql::MySqlPool;
 use sqlx::Error;
 use sqlx::FromRow;
+use thiserror::Error;
+use dotenv::dotenv;
+use std::env;
 
 #[derive(Debug, sqlx::FromRow)]
 struct Product {
-    id: u64,
+    id: u32,
     name: String,
     price: BigDecimal,
     description: Option<String>, // Добавлено поле описания, если оно есть в таблице
 }
 
-async fn fetch_all<T>(pool: &MySqlPool, table_name: &str) -> Result<Vec<T>, Error>
+#[derive(Error, Debug)]
+enum FetchError {
+    #[error("Database query failed: {0}")]
+    QueryError(#[from] Error),
+    #[error("No records found in table {0}")]
+    NoRecordsFound(String),
+    #[error("No record found with id {0}")]
+    NoRecordFound(u32),
+}
+
+async fn fetch_all<T>(pool: &MySqlPool, table_name: &str) -> Result<Vec<T>, FetchError>
 where
     T: for<'r> FromRow<'r, sqlx::mysql::MySqlRow> + Unpin + Send,
 {
@@ -19,17 +32,48 @@ where
     let items: Vec<T> = sqlx::query_as::<_, T>(&query)
         .fetch_all(pool)
         .await?;
+
+    if items.is_empty() {
+        return Err(FetchError::NoRecordsFound(table_name.to_string()));
+    }
+
     Ok(items)
 }
 
+async fn fetch_one<T>(pool: &MySqlPool, table_name: &str, id: u32) -> Result<T, FetchError>
+where
+    T: for<'r> FromRow<'r, sqlx::mysql::MySqlRow> + Unpin + Send,
+{
+    let query = format!("SELECT * FROM {} WHERE id = ?", table_name);
+    let item: Option<T> = sqlx::query_as::<_, T>(&query)
+        .bind(id)
+        .fetch_optional(pool)
+        .await?;
+
+    match item {
+        Some(record) => Ok(record),
+        None => Err(FetchError::NoRecordFound(id)),
+    }
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    let pool = MySqlPool::connect("mysql://homestead:secret@192.168.88.60/crlshop").await?;
+async fn main() -> Result<(), FetchError> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = MySqlPool::connect(&database_url).await?;
 
-    let products: Vec<Product> = fetch_all::<Product>(&pool, "products").await?;
+    match fetch_all::<Product>(&pool, "products").await {
+        Ok(products) => {
+            for product in products {
+                println!("{:?}", product);
+            }
+        }
+        Err(e) => println!("Error fetching products: {}", e),
+    }
 
-    for product in products {
-        println!("{:?}", product);
+    match fetch_one::<Product>(&pool, "products", 1).await {
+        Ok(product) => println!("Fetched product: {:?}", product),
+        Err(e) => println!("Error fetching product by ID: {}", e),
     }
 
     Ok(())
