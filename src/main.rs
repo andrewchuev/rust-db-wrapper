08e4a998cdac0c1d7ff561eb456d74da106e9b2e
Andrew A. Chuev <andrew.chuev@gmail.com>
2024-10-17 19:40:54 +0300

    Add fetch all wrapper

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
 use bigdecimal::BigDecimal;
 use sqlx::mysql::MySqlPool;
 use sqlx::Error;
+use sqlx::FromRow;
+
+#[derive(Debug, sqlx::FromRow)]
+struct Product {
+    id: u64,
+    name: String,
+    price: BigDecimal,
+    description: Option<String>, // Добавлено поле описания, если оно есть в таблице
+}
+
+async fn fetch_all<T>(pool: &MySqlPool, table_name: &str) -> Result<Vec<T>, Error>
+where
+    T: for<'r> FromRow<'r, sqlx::mysql::MySqlRow> + Unpin + Send,
+{
+    let query = format!("SELECT * FROM {}", table_name);
+    let items: Vec<T> = sqlx::query_as::<_, T>(&query)
+        .fetch_all(pool)
+        .await?;
+    Ok(items)
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-
     let pool = MySqlPool::connect("mysql://homestead:secret@192.168.88.60/crlshop").await?;
 
-    let row: (BigDecimal,) = sqlx::query_as("SELECT price FROM products WHERE id = ?")
-        .bind(1)
-        .fetch_one(&pool)
-        .await?;
+    let products: Vec<Product> = fetch_all::<Product>(&pool, "products").await?;
 
-    let price: BigDecimal = row.0;
-    println!("Цена: {}", price);
+    for product in products {
+        println!("{:?}", product);
+    }
 
     Ok(())
 }
